2cdeed668151acfd08e00e4b22e9e438a033c116
ChinoCoder <91900687+ChinoCoder@users.noreply.github.com>
2024-05-02 15:08:36 -0300

    initial commit

---
+++ src/main/kotlin/printscript/group13/snippetweb/SnippetRunnerApplication.kt
@@ -0,0 +1,11 @@
+package printscript.group13.snippetrunner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetRunnerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetRunnerApplication>(*args)
+}
---
+++ src/test/kotlin/printscript/group13/snippetrunner/SnippetRunnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package printscript.group13.snippetrunner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetRunnerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
