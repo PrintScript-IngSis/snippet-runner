c42f3b69182e728c37689179ee5106a7768dec41
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2024-07-19 04:41:51 -0300

    mock input

--- src/main/kotlin/printscript/group13/snippetrunner/input/InterpreterInput.kt
+++ src/main/kotlin/printscript/group13/snippetrunner/input/InterpreterInput.kt
@@ -3,6 +3,6 @@ package printscript.group13.snippetrunner.input
 data class InterpreterInput(
     val code: String,
     val version: String,
-    val inputs: List<String> = emptyList(),
+    val input: String = "",
     val env: Map<String, Any> = emptyMap(),
 )
